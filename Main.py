1b7613b70827df36801bf2722b0e19ea80923e69
Jonathan Shifman <jonathan.shifman@gmail.com>
2018-01-08 22:42:07 +0200

    Infrustracture for extracting information from pos_info

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ wrapper = MetaMapWrapper()
 sentence = 'Hypertension is a multifactorial disease involving the nervous, renal, ' \
            'and cardiovascular systems. '
 
-output_dict = wrapper.f(sentence)
+output_dict = wrapper.analyze_sentence(sentence)
 for key in output_dict:
     print key
     concept_dict = output_dict[key]
--- MetaMapWrapper.py
+++ MetaMapWrapper.py
@@ -11,7 +11,10 @@ class MetaMapWrapper(object):
         self.meta_map = MetaMap.get_instance(meta_map_path)
         self.relevant_field_names = config.get('general', 'relevant_field_names').split(',')
 
-    def f(self, sentence):
+    def extract_data_from_pos_info(self, pos_info):
+            return 'name', 1
+
+    def analyze_sentence(self, sentence):
         concepts, error = self.meta_map.extract_concepts([sentence], [1])
         output_dict = dict()
 
@@ -20,10 +23,11 @@ class MetaMapWrapper(object):
             concept_dict = dict()
             for field_name in self.relevant_field_names:
                 concept_dict[field_name] = getattr(concept, field_name)
+            original_name, starting_index = self.extract_data_from_pos_info(concept.pos_info)
+            concept_dict['original_name'] = original_name
+            concept_dict['starting_index'] = starting_index
+
             output_dict[current_index] = concept_dict
             current_index += 1
 
         return output_dict
-
-
-
